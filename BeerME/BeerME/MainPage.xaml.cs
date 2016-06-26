3518fae6e35a8bd130b9e2c6db019d9e18bc3dda
Cor <cbgunter@gmail.com>
2016-06-26 14:59:45 -0400

    Added GPIO functions

--- BeerME/BeerME/MainPage.xaml.cs
+++ BeerME/BeerME/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Gpio;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,71 @@ namespace BeerME
         public MainPage()
         {
             this.InitializeComponent();
+            this.InitGPIO();
+            this.IsPinAvailable();
+        }
+
+        private void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+
+            if (gpio == null)
+            {
+                IsGPIOAvail.Text = "There is no GPIO controller on this device.";
+                return;
+            }
+            else
+            {
+                IsGPIOAvail.Text = "The GPIO controller is available.";
+            }
+        }
+
+        private void IsPinAvailable()
+        {
+            var TempPin = GpioOpenStatus.PinOpened;
+
+            if (TempPin == 0)
+            {
+                IsPinAvail.Text = "The GPIO pin is available.";
+                ReadPinData();
+            }
+            else
+            {
+                IsPinAvail.Text = "The GPIO pin is not available.";
+                return;
+            }
+        }
+
+        private void ReadPinData()
+        {
+            const int PinNumber = 4;
+
+            var gpio = GpioController.GetDefault();
+            GpioPin TempPinInput = gpio.OpenPin(PinNumber);
+            
+            if (TempPinInput != null)
+            {
+                IsPinOpenDisplay.Text = "The pin was able to be opened.";
+
+                if (TempPinInput.Read() == GpioPinValue.High)
+                {
+                    PinReadDisplay.Text = "The pin is being read.";
+                    PinOutputDisplay.Text = GpioPinValue.High.ToString();
+                }
+
+                else
+                {
+                    PinReadDisplay.Text = "The pin is not being read.";
+                }
+                
+            }
+            
+            else
+            {
+                IsPinOpenDisplay.Text = "The pin was not able to be opened.";
+            }
+
+
         }
     }
 }
